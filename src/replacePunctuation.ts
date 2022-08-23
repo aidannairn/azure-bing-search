54151b6d5f5edb686b7ad5b84858cf83e61dde11
aidannairn <aidan.nairn95@gmail.com>
2022-08-23 13:31:24 +1200

    feat(replacePunctuation): Use regex to filter punctuation and consecutive spaces out of string then return the result.

--- src/replacePunctuation.ts
+++ src/replacePunctuation.ts
@@ -1,5 +1,17 @@
 const replacePunctuation = (testStr: string): string => {
-  return testStr
+  const punctuationRegex = /[!!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
+  const multipleSpaceRegex = /  +/g
+
+  // Replace punctuation with a space
+  const filteredPunctuationResult = testStr.replace(punctuationRegex, ' ')
+
+  // Replace consecutive spaces with a single space
+  const filteredSpacesResult = filteredPunctuationResult.replace(multipleSpaceRegex, ' ')
+
+  // Remove leading and trailing spaces
+  const trimmedResult = filteredSpacesResult.trim()
+
+  return trimmedResult
 }
 
 export default replacePunctuation
